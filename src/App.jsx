8e8b3f41ad7f49d0baf6cef0fd1db2e0219b2c12
Erike Nascimento <nascimento.lf.erike@gmail.com>
2024-07-27 20:38:03 -0300

    Inserting the context provider into de app.jsx file and filling it with the app routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { useState, useEffect } from "react";
 import Home from "./pages/Home";
 import Carrinho from "./pages/Carrinho";
 import PaginaErro from "./pages/PaginaErro";
 
 import "./App.css";
+import { CarrinhoProvider } from "./context/CarrinhoContext";
 
 function App() {
 	return (
 		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/carrinho" element={<Carrinho />} />
-				<Route path="*" element={<PaginaErro />} />
-			</Routes>
+			<CarrinhoProvider>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/carrinho" element={<Carrinho />} />
+					<Route path="*" element={<PaginaErro />} />
+				</Routes>
+			</CarrinhoProvider>
 		</BrowserRouter>
 	);
 }
