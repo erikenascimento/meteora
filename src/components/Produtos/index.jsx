0a24aed592430b04c32db7d89876bb96cf24a031
Erike Nascimento <nascimento.lf.erike@gmail.com>
2024-07-27 20:47:27 -0300

    importing the shopping cart context using the useContext hook

--- src/components/Produtos/index.jsx
+++ src/components/Produtos/index.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
 import Produto from "./Produto";
 import produtos from "@/mocks/produtos.json";
 import Titulo from "@/components/Titulo";
+import { CarrinhoContext } from "@/context/CarrinhoContext";
 
 const Produtos = () => {
+	const { carrinho, setCarrinho } = useContext(CarrinhoContext);
 	return (
 		<section role="produtos" aria-label="Produtos que estão bombando!">
 			<Titulo>Produtos que estão bombando!</Titulo>
