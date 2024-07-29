969f47f9868e47be89ab2d875171464c5e0e0ed0
Erike Nascimento <nascimento.lf.erike@gmail.com>
2024-07-29 14:54:25 -0300

    Using the useMemo hook to memorize the calculation of the shopping cart to improve performance

--- src/hooks/useCarrinhoContext.js
+++ src/hooks/useCarrinhoContext.js
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useMemo } from "react";
 import { CarrinhoContext } from "@/context/CarrinhoContext";
 
 export const useCarrinhoContext = () => {
@@ -56,8 +56,8 @@ export const useCarrinhoContext = () => {
 		setCarrinho(produto);
 	}
 
-	useEffect(() => {
-		const { totalTemp, quantidadeTemp } = carrinho.reduce(
+	const { totalTemp, quantidadeTemp } = useMemo(() => {
+		return carrinho.reduce(
 			() => (acumulador, produto) => ({
 				quantidadeTemp: acumulador.quantidadeTemp + produto.quantidade,
 				totalTemp: acumulador.totalTemp + produto.preco * produto.quantidade,
@@ -67,9 +67,12 @@ export const useCarrinhoContext = () => {
 				totalTemp: 0,
 			}
 		);
+	}, [carrinho]);
+
+	useEffect(() => {
 		setQuantidade(quantidadeTemp);
 		setValorTotal(totalTemp);
-	}, [carrinho]);
+	});
 
 	return {
 		carrinho,
